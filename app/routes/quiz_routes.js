57e916c9d69bf9ca50ffeb735a73377b88ac7f96
iamstrangepants <feliciano.madaline@gmail.com>
2018-09-08 20:59:03 -0500

    yay

---
+++ app/routes/quiz_routes.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var apiRouter = express.Router();
+var quiz = require('./../quiz.js');
+
+apiRouter.get('/', function(req, res) {
+  res.json ({ message: 'hooray! your API is working'});
+});
+
+apiRouter.get('/category/:category', function(req, res) {
+ quiz
+   .getQuiz(req.params)
+   .then(function(result){res.json(result);});
+});
+
+apiRouter.get('/category/:category/difficulty/:difficulty', function(req, res) {
+ quiz
+   .getQuiz(req.params)
+   .then(function(result){res.json(result);});
+});
+
+apiRouter.get('/category/:category/difficulty/:difficulty/count/:count', function(req, res) {
+ quiz
+   .getQuiz(req.params)
+   .then(function(result){res.json(result);});
+});
+
+module.exports = apiRouter;
